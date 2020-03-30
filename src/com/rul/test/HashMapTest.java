5020e3293f55e932c1463fa56d9106b38b29f8f0
luoru0221 <331980169@qq.com>
2020-03-30 11:08:31 +0800

    init

---
+++ src/com/rul/test/HashMapTest.java
@@ -0,0 +1,13 @@
+package com.rul.test;
+
+import java.util.HashMap;
+
+public class HashMapTest {
+
+    public static void main(String[] args) {
+        HashMap<String,String> map = new HashMap<>();
+        map.put("123","123");
+        System.out.println(map);
+        System.out.println("hhh");
+    }
+}
